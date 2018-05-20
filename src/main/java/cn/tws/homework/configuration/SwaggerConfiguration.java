a5d167af84fcb08f2e70e612787af73b8e47eb76
xiazihang <zhxia@thoughtworks.com>
2018-05-20 11:35:05 +0800

    set up

---
+++ src/main/java/cn/tws/homework/configuration/SwaggerConfiguration.java
@@ -0,0 +1,50 @@
+package cn.tws.homework.configuration;
+
+import cn.tws.homework.swagger.SwaggerDocketFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+    private static final String CONTROLLER_PACKAGE = "cn.tws.homework";
+
+    @Value("${info.version}")
+    private String version;
+
+    @Value("${info.app.name}")
+    private String name;
+
+    @Value("${info.app.description}")
+    private String description;
+
+    @Value("${info.contact.author}")
+    private String author;
+
+    @Value("${info.contact.email}")
+    private String email;
+
+    @Value("${info.app.protocol:https}")
+    private String protocol;
+
+    @Bean
+    public Docket createDocket() {
+        return SwaggerDocketFactory.createDocket(CONTROLLER_PACKAGE, name, apiInfo(), protocol);
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact(author, "", email);
+        return new ApiInfoBuilder()
+                .title(name)
+                .description(description)
+                .contact(contact)
+                .version(version)
+                .build();
+
+    }
+
+}
